9c1d779972a210e4807e3130019d87711dbe565e
dshin21 <danielshin234@gmail.com>
2019-01-25 16:45:55 -0800

    saved values to the localstorage

--- lab3/lab3.js
+++ lab3/lab3.js
@@ -10,10 +10,10 @@ addBtn.onclick = () => {
     <div>Question Text: </div>
     <textarea class='qText${i}' rows="4" cols="50"></textarea><br/>                
     <div>Answers: </div>
-    <input type="radio" name="gender${i}" value="male"> <input type="text" name="FirstName${i}"><br/>  
-    <input type="radio" name="gender${i}" value="male"> <input type="text" name="FirstName${i}"><br/>  
-    <input type="radio" name="gender${i}" value="male"> <input type="text" name="FirstName${i}"><br/>  
-    <input type="radio" name="gender${i}" value="male"> <input type="text" name="FirstName${i}"><br/>
+    <input type="radio" name="q${i}"> <input type="text" name="FirstName${i}"><br/>  
+    <input type="radio" name="q${i}"> <input type="text" name="FirstName${i}"><br/>  
+    <input type="radio" name="q${i}"> <input type="text" name="FirstName${i}"><br/>  
+    <input type="radio" name="q${i}"> <input type="text" name="FirstName${i}"><br/>
     <button type="button" id="${i}" class="deleteBtn">Delete</button><br/>
     </div>`;
   questionBank.push(oneQAset);
@@ -39,8 +39,37 @@ const reRenderDelete = id => {
 };
 
 saveBtn.onclick = () => {
-  //TODO: save form inputs
+  let tempArr = [];
+  for (let k = 0; k < i; ++k) {
+    let temp = {
+      textArea: $(`#Q${k + 1}`)[k].children[1].value,
+      radio1: $(`#Q${k + 1}`)[k].children[4].checked,
+      input1: $(`#Q${k + 1}`)[k].children[5].value,
+      radio2: $(`#Q${k + 1}`)[k].children[7].checked,
+      input2: $(`#Q${k + 1}`)[k].children[8].value,
+      radio3: $(`#Q${k + 1}`)[k].children[10].checked,
+      input3: $(`#Q${k + 1}`)[k].children[11].value,
+      radio4: $(`#Q${k + 1}`)[k].children[13].checked,
+      input4: $(`#Q${k + 1}`)[k].children[14].value
+    };
+    tempArr.push(temp);
+  }
+  localStorage.setItem("questionBank", JSON.stringify(tempArr));
+
+  // console.log($("#Q1")[0].children[1].value); //text area
+
+  // console.log($("#Q1")[0].children[4].checked); //radio1
+  // console.log($("#Q1")[0].children[5].value); //input1
+
+  // console.log($("#Q1")[0].children[7].checked); //radio2
+  // console.log($("#Q1")[0].children[8].value); //input2
+
+  // console.log($("#Q1")[0].children[10].checked); //radio3
+  // console.log($("#Q1")[0].children[11].value); //input3
+
+  // console.log($("#Q1")[0].children[13].checked); //radio4
+  // console.log($("#Q1")[0].children[14].value); //input4
+
   alert("Saved!");
 };
 
-// localStorage.getItem("questionBank");
