713cd723f65034d473be8d8dc2ec310cd52ad010
dshin21 <danielshin234@gmail.com>
2019-01-28 16:49:42 -0800

    add/delete working

--- lab3/admin.js
+++ lab3/admin.js
@@ -1,75 +1,90 @@
-let main = $("#main")[0];
+localStorage.clear();
+let main = $("#main");
 let addBtn = $("#addBtn")[0];
 let saveBtn = $("#saveBtn")[0];
-let questionBank = [];
 let i = 0;
+let questionBank = [];
 
-addBtn.onclick = () => {
-  i++;
+const updateLocalStorage = () => {
+  localStorage.setItem("questionBank", JSON.stringify(questionBank));
+  questionBank = JSON.parse(localStorage.getItem("questionBank"));
+  // i = questionBank.length;
+};
+
+const retrieveLocalStorage = () => {
+  if (JSON.parse(localStorage.getItem("questionBank")) == null) {
+    questionBank = [];
+  } else {
+    questionBank = JSON.parse(localStorage.getItem("questionBank"));
+  }
+  // i = questionBank.length;
+};
+
+const addNewQuestion = () => {
+  retrieveLocalStorage();
   let oneQAset = `<div id='Q${i}'>
-    <div>Question Text: </div>
-    <textarea class='qText${i}' rows="4" cols="50"></textarea><br/>                
-    <div>Answers: </div>
-    <input type="radio" name="q${i}"> <input type="text" name="FirstName${i}"><br/>  
-    <input type="radio" name="q${i}"> <input type="text" name="FirstName${i}"><br/>  
-    <input type="radio" name="q${i}"> <input type="text" name="FirstName${i}"><br/>  
-    <input type="radio" name="q${i}"> <input type="text" name="FirstName${i}"><br/>
-    <button type="button" id="${i}" class="deleteBtn">Delete</button><br/>
-    </div>`;
+  <div>Question Text: </div>
+  <textarea class='textArea' rows="4" cols="50"></textarea><br/>                
+  <div>Answers: </div>
+  <input type="radio" name="q${i}"> <input type="text" class="input1" name="FirstName${i}"><br/>  
+  <input type="radio" name="q${i}"> <input type="text" class="input2" name="FirstName${i}"><br/>  
+  <input type="radio" name="q${i}"> <input type="text" class="input3" name="FirstName${i}"><br/>  
+  <input type="radio" name="q${i}"> <input type="text" class="input4" name="FirstName${i}"><br/>
+  <button type="button" id="${i}" class="deleteBtn">Delete</button><br/>
+  </div>`;
   questionBank.push(oneQAset);
-  reRenderAdd();
+  i++;
+  updateLocalStorage();
+  reRender();
+  addDeleteBtn();
+};
 
+const reRender = () => {
+  retrieveLocalStorage();
+
+  main.append(questionBank[questionBank.length - 1]);
+  updateLocalStorage();
+  downloadValues();
+};
+
+const downloadValues = () => {
+  retrieveLocalStorage();
+  for (let k = 0; k < i; k++) {
+    // console.log("downloading: " + k);
+    if ($(`.textArea`)[k]) $(`.textArea`)[k].value = questionBank[k].textArea;
+    if ($(`.input1`)[k]) $(`.input1`)[k].value = questionBank[k].input1;
+    if ($(`.input2`)[k]) $(`.input2`)[k].value = questionBank[k].input2;
+    if ($(`.input3`)[k]) $(`.input3`)[k].value = questionBank[k].input3;
+    if ($(`.input4`)[k]) $(`.input4`)[k].value = questionBank[k].input4;
+  }
+  updateLocalStorage();
+};
+
+const addDeleteBtn = () => {
   for (let k = 0; k < $(".deleteBtn").length; k++) {
     let deleteBtn = $(".deleteBtn")[k];
 
     deleteBtn.onclick = () => {
-      reRenderDelete(deleteBtn.id);
+      console.log("deleting id: " + deleteBtn.id);
+      questionBank.forEach(e => {
+        let temp = e.substring(10, 12);
+        if (deleteBtn.id === temp.charAt(0) || deleteBtn.id === e.substring(10,12)) {
+          console.log("deleting id: " + deleteBtn.id);
+          console.log(questionBank);
+          questionBank.filter((value, index, arr) => {
+            console.log(e.charAt(10));
+          });
+          $(`#Q${deleteBtn.id}`)[0].remove();
+          updateLocalStorage();
+        }
+      });
+      updateLocalStorage();
     };
   }
 };
 
-const reRenderAdd = () => {
-  main.innerHTML += questionBank[questionBank.length - 1];
-};
+addBtn.onclick = addNewQuestion;
+// create 2 functions, update localstorage & retrieve local storage
+// use all values from the localstorage
 
-const reRenderDelete = id => {
-  let deleteBtn = document.getElementById(`Q${id}`);
-  deleteBtn.remove();
-  if (i > 0) i--;
-};
-
-saveBtn.onclick = () => {
-  let tempArr = [];
-  for (let k = 0; k < i; ++k) {
-    let temp = {
-      textArea: $(`#Q${k + 1}`)[0].children[1].value,
-      radio1: $(`#Q${k + 1}`)[0].children[4].checked,
-      input1: $(`#Q${k + 1}`)[0].children[5].value,
-      radio2: $(`#Q${k + 1}`)[0].children[7].checked,
-      input2: $(`#Q${k + 1}`)[0].children[8].value,
-      radio3: $(`#Q${k + 1}`)[0].children[10].checked,
-      input3: $(`#Q${k + 1}`)[0].children[11].value,
-      radio4: $(`#Q${k + 1}`)[0].children[13].checked,
-      input4: $(`#Q${k + 1}`)[0].children[14].value
-    };
-    tempArr.push(temp);
-  }
-  localStorage.setItem("questionBank", JSON.stringify(tempArr));
-
-  // console.log($("#Q1")[0].children[1].value); //text area
-
-  // console.log($("#Q1")[0].children[4].checked); //radio1
-  // console.log($("#Q1")[0].children[5].value); //input1
-
-  // console.log($("#Q1")[0].children[7].checked); //radio2
-  // console.log($("#Q1")[0].children[8].value); //input2
-
-  // console.log($("#Q1")[0].children[10].checked); //radio3
-  // console.log($("#Q1")[0].children[11].value); //input3
-
-  // console.log($("#Q1")[0].children[13].checked); //radio4
-  // console.log($("#Q1")[0].children[14].value); //input4
-
-  alert("Saved!");
-};
-
+// TODO: same Q class names -> delete id by iterating
